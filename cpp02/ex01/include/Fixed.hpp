a793543dd499825b23435cb24cffb2b95f5094d6
Alisson Marcos <alissonmarcos249@gmail.com>
2024-08-26 01:42:18 -0300

    cpp02/ex01 done

--- cpp02/ex01/include/Fixed.hpp
+++ cpp02/ex01/include/Fixed.hpp
@@ -1,16 +1,25 @@
 #pragma once
 
+#include <iosfwd>
+
 class Fixed
 {
 public:
   Fixed ();
-  Fixed (const Fixed &object);
   ~Fixed ();
+  Fixed (const Fixed &object);
+  Fixed (const int Value);
+  int toInt() const;
+  Fixed (const float Value);
+  float toFloat() const;
   Fixed &operator= (const Fixed &object);
   int getRawBits (void) const;
   void setRawBits (int const raw);
 
 private:
-  int integer;
-  const static int _bits = 8;
+  int _rawBits;
+  const static int _bitsOffset = 8;
 };
+
+std::ostream&
+operator<<(std::ostream &out, const Fixed &objec);
--- cpp02/ex01/src/Fixed.cpp
+++ cpp02/ex01/src/Fixed.cpp
@@ -1,9 +1,34 @@
 #include "Fixed.hpp"
 #include <iostream>
+#include <cmath>
 
-Fixed::Fixed () : integer (0)
+Fixed::Fixed () : _rawBits (0)
 {
   std::cout << "Dafault constructor called" << std::endl;
+}
+
+Fixed::Fixed (const int Value)
+{
+  std::cout << "Int constructor called" << std::endl;
+  this->_rawBits = Value << Fixed::_bitsOffset;
+}
+
+int
+Fixed::toInt () const
+{
+  return this->_rawBits >> Fixed::_bitsOffset;
+}
+
+Fixed::Fixed (const float Value)
+{
+  std::cout << "Float constructor called" << std::endl;
+  this->_rawBits = roundf(Value * (1 << Fixed::_bitsOffset));
+}
+
+float
+Fixed::toFloat () const
+{
+  return (float) this->_rawBits / (1 << Fixed::_bitsOffset);
 }
 
 Fixed &
@@ -11,8 +36,15 @@ Fixed::operator= (const Fixed &object)
 {
   std::cout << "Copy assigment operator called" << std::endl;
   if (this != &object)
-    integer = object.getRawBits ();
+    _rawBits = object.getRawBits ();
   return *this;
+}
+
+std::ostream &
+operator<<(std::ostream &out, const Fixed &object)
+{
+  out << object.toFloat();
+  return out;
 }
 
 Fixed::Fixed (const Fixed &object)
@@ -27,12 +59,12 @@ int
 Fixed::getRawBits (void) const
 {
   std::cout << "getRawBits member function called" << std::endl;
-  return integer;
+  return _rawBits;
 }
 
 void
 Fixed::setRawBits (const int bits)
 {
   std::cout << "setRawBits member function called" << std::endl;
-  integer = bits;
+  _rawBits = bits;
 }
