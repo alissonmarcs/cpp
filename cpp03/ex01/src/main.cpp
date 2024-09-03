a30ec5942bf8285cea36dd0d822cc1e16fe5a8b4
Alisson <alissonmarcos249@gmail.com>
2024-09-03 19:06:19 -0300

    wip

--- cpp03/ex01/src/main.cpp
+++ cpp03/ex01/src/main.cpp
@@ -1,27 +1,100 @@
 #include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
 #include <iostream>
+#include <vector>
 
-int
-main (void)
+// int
+// main (void)
+// {
+//   ScavTrap scavTrap ("carla");
+//   ClapTrap clapTrap ("simone");
+
+//   print ("ClapTrap name: " << clapTrap.getName ());
+//   print ("ScavTrap name: " << scavTrap.getName ());
+
+//   print ("ClapTrap hit points: " << clapTrap.getHitPoints ());
+//   print ("ScavTrap hit points: " << scavTrap.getHitPoints ());
+
+//   print ("ClapTrap energy points: " << clapTrap.getEnergyPoints ());
+//   print ("ScavTrap energy points: " << scavTrap.getEnergyPoints ());
+
+//   print ("ClapTrap attack damage: " << clapTrap.getAttackDamage ());
+//   print ("ScavTrap attack damage: " << scavTrap.getAttackDamage ());
+
+//   clapTrap.attack ("enemy");
+//   scavTrap.attack ("enemy");
+
+//   scavTrap.guardGate ();
+// }
+
+// namespace Tests
+// {
+//   void TestCopyConstructor()
+//   {
+//     ClapTrap clap("carla");
+//     ScavTrap scav(clap);
+
+
+//     print("Scav Name: " << scav.getName());
+//     print("Scav Hit Points: " << scav.getHitPoints());
+//     print("Scav Energy Points: " << scav.getEnergyPoints());
+//     print("Scav Attack Damage: " << scav.getAttackDamage());
+//   }
+
+// }
+
+class Base
 {
-  ScavTrap scavTrap ("carla");
-  ClapTrap clapTrap ("simone");
+protected:
+  std::string _name;
 
-  print ("ClapTrap name: " << clapTrap.getName ());
-  print ("ScavTrap name: " << scavTrap.getName ());
+public:
+  Base(std::string name) : _name(name) {}
+  std::string get_name()
+  {
+    return  _name;
+  }
+};
 
-  print ("ClapTrap hit points: " << clapTrap.getHitPoints ());
-  print ("ScavTrap hit points: " << scavTrap.getHitPoints ());
+class Circle: public Base
+{
+public:
+  Circle(std::string name) : Base(name) {}
+  void print_form()
+  {
+    std::cout << "Circle named: " << get_name() << std::endl;
+  }
+};
 
-  print ("ClapTrap energy points: " << clapTrap.getEnergyPoints ());
-  print ("ScavTrap energy points: " << scavTrap.getEnergyPoints ());
+class Triangle: public Base
+{
+public:
+  Triangle(std::string name) : Base(name) {}
+  void print_form()
+  {
+    std::cout << "Triangle named: " << get_name() << std::endl;
+  }
+};
 
-  print ("ClapTrap attack damage: " << clapTrap.getAttackDamage ());
-  print ("ScavTrap attack damage: " << scavTrap.getAttackDamage ());
+int main()
+{
+  std::vector<Base *> vect;
+  Circle circle("circle");
+  Triangle triangle("triangle");
 
-  clapTrap.attack ("enemy");
-  scavTrap.attack ("enemy");
+  vect.push_back(&circle);
+  vect.push_back(&triangle);
 
-  scavTrap.guardGate ();
+  ((Circle *)vect[0])->print_form();
+  ((Triangle *)vect[1])->print_form();
+  return 0;
 }
+
+// int main()
+// {
+//   ScavTrap scavTrap("carla");
+//   ClapTrap tmp(scavTrap);
+
+//   print(tmp.getAttackDamage());
+//   return 0;
+// }
