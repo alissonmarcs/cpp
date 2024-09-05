581f5c17b5bf71ceb01b4b91a81ec7fe3cdecf22
Alisson <alissonmarcos249@gmail.com>
2024-09-05 08:16:40 -0300

    cpp03 ex01 format

--- cpp03/ex01/src/main.cpp
+++ cpp03/ex01/src/main.cpp
@@ -4,79 +4,95 @@
 
 namespace Tests
 {
-  void TestDefaultConstructor()
-  {
-    ScavTrap scav;
+void
+TestDefaultConstructor ()
+{
+  ScavTrap scav;
 
-    print(std::setw(30) << std::left << "scav Name: " << scav.getName());
-    print(std::setw(30) << std::left << "scav Hit Points: " << scav.getHitPoints());
-    print(std::setw(30) << std::left << "scav Energy Points: " << scav.getEnergyPoints());
-    print(std::setw(30) << std::left << "scav Attack Damage: " << scav.getAttackDamage());
-  }
+  print (std::setw (30) << std::left << "scav Name: " << scav.getName ());
+  print (std::setw (30) << std::left
+                        << "scav Hit Points: " << scav.getHitPoints ());
+  print (std::setw (30) << std::left
+                        << "scav Energy Points: " << scav.getEnergyPoints ());
+  print (std::setw (30) << std::left
+                        << "scav Attack Damage: " << scav.getAttackDamage ());
+}
 
-  void TestNameConstructor()
-  {
-    ScavTrap scav ("senhor pedro");
+void
+TestNameConstructor ()
+{
+  ScavTrap scav ("senhor pedro");
 
-    print(std::setw(30) << std::left << "scav Name: " << scav.getName());
-    print(std::setw(30) << std::left << "scav Hit Points: " << scav.getHitPoints());
-    print(std::setw(30) << std::left << "scav Energy Points: " << scav.getEnergyPoints());
-    print(std::setw(30) << std::left << "scav Attack Damage: " << scav.getAttackDamage());
-  }
+  print (std::setw (30) << std::left << "scav Name: " << scav.getName ());
+  print (std::setw (30) << std::left
+                        << "scav Hit Points: " << scav.getHitPoints ());
+  print (std::setw (30) << std::left
+                        << "scav Energy Points: " << scav.getEnergyPoints ());
+  print (std::setw (30) << std::left
+                        << "scav Attack Damage: " << scav.getAttackDamage ());
+}
 
-  void TestCopyConstructor()
-  {
-    ScavTrap scav ("carla");
-    ScavTrap scavCopy (scav);
+void
+TestCopyConstructor ()
+{
+  ScavTrap scav ("carla");
+  ScavTrap scavCopy (scav);
 
+  print (std::setw (30) << std::left << "scavCopy Name: " << scav.getName ());
+  print (std::setw (30) << std::left
+                        << "scavCopy Hit Points: " << scav.getHitPoints ());
+  print (std::setw (30) << std::left << "scavCopy Energy Points: "
+                        << scav.getEnergyPoints ());
+  print (std::setw (30) << std::left << "scavCopy Attack Damage: "
+                        << scav.getAttackDamage ());
+}
 
-    print(std::setw(30) << std::left << "scavCopy Name: " << scav.getName());
-    print(std::setw(30) << std::left << "scavCopy Hit Points: " << scav.getHitPoints());
-    print(std::setw(30) << std::left << "scavCopy Energy Points: " << scav.getEnergyPoints());
-    print(std::setw(30) << std::left << "scavCopy Attack Damage: " << scav.getAttackDamage());
-  }
+void
+TestAssignmentOperator ()
+{
+  ScavTrap scav ("carla");
+  ScavTrap scavCopy ("simone");
 
-  void TestAssignmentOperator()
-  {
-    ScavTrap scav ("carla");
-    ScavTrap scavCopy ("simone");
+  scav.setHitPoints (3);
+  scav.setEnergyPoints (3);
+  scav.setAttackDamage (3);
+  scavCopy = scav;
+  print (std::setw (30) << std::left << "scavCopy Name: " << scav.getName ());
+  print (std::setw (30) << std::left
+                        << "scavCopy Hit Points: " << scav.getHitPoints ());
+  print (std::setw (30) << std::left << "scavCopy Energy Points: "
+                        << scav.getEnergyPoints ());
+  print (std::setw (30) << std::left << "scavCopy Attack Damage: "
+                        << scav.getAttackDamage ());
+}
 
-    scav.setHitPoints(3);
-    scav.setEnergyPoints(3);
-    scav.setAttackDamage(3);
-    scavCopy = scav;
-    print(std::setw(30) << std::left << "scavCopy Name: " << scav.getName());
-    print(std::setw(30) << std::left << "scavCopy Hit Points: " << scav.getHitPoints());
-    print(std::setw(30) << std::left << "scavCopy Energy Points: " << scav.getEnergyPoints());
-    print(std::setw(30) << std::left << "scavCopy Attack Damage: " << scav.getAttackDamage());
-  }
+void
+TestScavTrapSpecifics ()
+{
+  ScavTrap scav ("carla");
 
-  void TestScavTrapSpecifics()
-  {
-    ScavTrap scav ("carla");
-
-    scav.guardGate();
-    scav.attack("enemy");
-  }
+  scav.guardGate ();
+  scav.attack ("enemy");
+}
 }
 
 int
-main()
+main ()
 {
-  print("\n\n\t\tTesting Default Constructor\n");
-  Tests::TestDefaultConstructor();
+  print ("\n\n\t\tTesting Default Constructor\n");
+  Tests::TestDefaultConstructor ();
 
-  print("\n\n\t\tTesting Name Constructor\n");
-  Tests::TestNameConstructor();
+  print ("\n\n\t\tTesting Name Constructor\n");
+  Tests::TestNameConstructor ();
 
-  print("\n\n\t\tTesting Copy Constructor\n");
-  Tests::TestCopyConstructor();
+  print ("\n\n\t\tTesting Copy Constructor\n");
+  Tests::TestCopyConstructor ();
 
-  print("\n\n\t\tTesting Assignment Operator\n");
-  Tests::TestAssignmentOperator();
+  print ("\n\n\t\tTesting Assignment Operator\n");
+  Tests::TestAssignmentOperator ();
 
-  print("\n\n\t\tTesting ScavTrap specifics\n");
-  Tests::TestScavTrapSpecifics();
+  print ("\n\n\t\tTesting ScavTrap specifics\n");
+  Tests::TestScavTrapSpecifics ();
 
   return 0;
 }
