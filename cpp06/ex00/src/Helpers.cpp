85a57ff8be688090a62bf0e6e590dadc953b3050
Alisson Marcos <alissonmarcos249@gmail.com>
2024-10-24 13:30:15 -0300

    reorganizacao namespace Helpers

--- cpp06/ex00/src/Helpers.cpp
+++ cpp06/ex00/src/Helpers.cpp
@@ -10,6 +10,52 @@ namespace Helpers
 {
 
 bool
+validade_string (std::string str)
+{
+  int dot = 0;
+  size_t i = 0;
+
+  if (isPseudoLiteral (str))
+    return true;
+  if ((std::isalpha (str[0]) && str.size () == 1))
+    return true;
+  if (str[i] == '-' || str[i] == '+')
+    i++;
+  if (!std::isdigit (str[i]) && str.size () > 1)
+    return false;
+  if (str.empty ())
+    return false;
+  for (; i < str.size (); i++)
+    {
+      if (str[i] == '.')
+        dot++;
+      if (!std::isdigit (str[i]) && str[i] != '.' && i != str.size () - 1)
+        return false;
+      if (!std::isdigit (str[i]) && i == str.size () - 1 && str[i] != 'f')
+        return false;
+    }
+  if (str[str.size () - 1] == 'f' && dot != 1)
+    return false;
+  return true;
+}
+
+int
+getType (std::string str)
+{
+  if (isChar (str))
+    return CHAR;
+  else if (isInt (str))
+    return INT;
+  else if (isFloat (str))
+    return FLOAT;
+  else if (isDouble (str))
+    return DOUBLE;
+  else if (isPseudoLiteral (str))
+    return PSEUDO_LITERAL;
+  return -1;
+}
+
+bool
 isFloat (std::string str)
 {
   int dot = 0;
@@ -67,36 +113,6 @@ isChar (std::string str)
 }
 
 bool
-validade_string (std::string str)
-{
-  int dot = 0;
-  size_t i = 0;
-
-  if (isPseudoLiteral (str))
-    return true;
-  if ((std::isalpha (str[0]) && str.size () == 1))
-    return true;
-  if (str[i] == '-' || str[i] == '+')
-    i++;
-  if (!std::isdigit (str[i]) && str.size () > 1)
-    return false;
-  if (str.empty ())
-    return false;
-  for (; i < str.size (); i++)
-    {
-      if (str[i] == '.')
-        dot++;
-      if (!std::isdigit (str[i]) && str[i] != '.' && i != str.size () - 1)
-        return false;
-      if (!std::isdigit (str[i]) && i == str.size () - 1 && str[i] != 'f')
-        return false;
-    }
-  if (str[str.size () - 1] == 'f' && dot != 1)
-    return false;
-  return true;
-}
-
-bool
 isPseudoLiteral (std::string str)
 {
   std::string literals[]
@@ -110,46 +126,16 @@ isPseudoLiteral (std::string str)
   return false;
 }
 
-int
-getType (std::string str)
-{
-  if (isChar (str))
-    return CHAR;
-  else if (isInt (str))
-    return INT;
-  else if (isFloat (str))
-    return FLOAT;
-  else if (isDouble (str))
-    return DOUBLE;
-  else if (isPseudoLiteral (str))
-    return PSEUDO_LITERAL;
-  return -1;
-}
-
 void
 printPseudoLiteral (std::string str)
 {
     print (PURPLE << "char: " << RESET << "impossible");
     print (PURPLE << "int: " << RESET << "impossible");
-    print (PURPLE << "float: " << RESET << (str + "f"));
-    print (PURPLE << "double: " << RESET << str);
-}
-
-void
-convertFromChar (std::string str)
-{
-    char c = static_cast<char>(str[0]);
-    int i = static_cast<int>(c);
-    float f = static_cast<float>(c);
-    double d = static_cast<double>(c);
-
-    if (std::isprint(c))
-        print (PURPLE << "char: " << RESET << c);
+    if (str[str.size() - 1] == 'f')
+        print (PURPLE << "float: " << RESET << str);
     else
-        print (PURPLE << "char: " << RESET << "Non displayable");
-    print (PURPLE << "int: " << RESET << i);
-    print (PURPLE << "float: " << RESET << std::fixed << std::setprecision(1) << f << "f");
-    print (PURPLE << "double: " << RESET << std::fixed << std::setprecision(1) << d);
+        print (PURPLE << "float: " << RESET << (str + "f"));
+    print (PURPLE << "double: " << RESET << str);
 }
 
 void
@@ -185,15 +171,31 @@ convertFromInt (std::string str)
 }
 
 void
+convertFromChar (std::string str)
+{
+    char c = static_cast<char>(str[0]);
+    int i = static_cast<int>(c);
+    float f = static_cast<float>(c);
+    double d = static_cast<double>(c);
+
+    if (std::isprint(c))
+        print (PURPLE << "char: " << RESET << c);
+    else
+        print (PURPLE << "char: " << RESET << "Non displayable");
+    print (PURPLE << "int: " << RESET << i);
+    print (PURPLE << "float: " << RESET << std::fixed << std::setprecision(1) << f << "f");
+    print (PURPLE << "double: " << RESET << std::fixed << std::setprecision(1) << d);
+}
+
+void
 convertFromFloat (std::string str)
 {
-    double toCheck = std::strtod(str.c_str(), NULL);
+  double toCheck = std::strtod(str.c_str(), NULL);
     if (toCheck > std::numeric_limits<float>::max() || toCheck < std::numeric_limits<float>::min())
     {
         printAllImpossible();
         return;
     }
-  
     float f = strtof(str.c_str(), NULL);
     double d = static_cast<double>(f);
     int i = static_cast<int>(d);
@@ -212,7 +214,7 @@ void
 convertFromDouble (std::string str)
 {
     double toCheck = std::strtod(str.c_str(), NULL);
-    if (toCheck > std::numeric_limits<float>::max() || toCheck < std::numeric_limits<float>::min())
+    if (toCheck > std::numeric_limits<int>::max() || toCheck < std::numeric_limits<int>::min())
     {
         printAllImpossible();
         return;
