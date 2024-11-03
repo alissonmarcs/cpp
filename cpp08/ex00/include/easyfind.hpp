8fea5f272cda48b4d1277a0fc0a34a2b11e8aad1
Alisson <alissonmarcos249@gmail.com>
2024-11-03 10:50:09 -0300

    cpp08 done

---
+++ cpp08/ex00/include/easyfind.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <algorithm>
+#include <iterator>
+#include <stdexcept>
+
+#include "Defines.hpp"
+
+template <typename T>
+typename T::iterator easyfind(T &container, int target)
+{
+    typename T::iterator it;
+
+    it = std::find(container.begin(), container.end(), target);
+    if (it == container.end())
+        throw std::out_of_range(RED "Element not found" RESET);
+    return it;
+}
